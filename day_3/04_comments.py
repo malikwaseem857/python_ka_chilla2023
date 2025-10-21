9128118f9550a566b4c682a29e577f25eecdf544
waseem857 <malikwaseem3802@gmail.com>
2025-10-21 09:49:20 +0500

    Update author and contact information in comments

--- day_3/04_comments.py
+++ day_3/04_comments.py
@@ -1,8 +1,8 @@
 # whenever you start any thing in the system, it will be commented
 #Subject of this python scirpt
-#Author of this python script
-# Where to contact: github profile link or email(gmail)
-#Date of creation
+#Author of this python script: Waseem Akram
+# Where to contact: github profile link(https://github.com/malikwaseem857) or email(malikwaseem3802@gmail.com)
+#Date of creation: 21-10-2025
 
 
 # Step-1: Why to use single quotation?
@@ -18,6 +18,6 @@ print("What's up")
 print('''
       Waseem's
       Akram
-      ''') # to write multiline string and also using """or" quotation marks insdie )
+      ''') # to write multiline string and also using """ or" quotation marks insdie )
 
 # Assignment: When to use comments in python 10 study cases.
