5f5531b644c53d2bd8f455be1517668663b6c499
malikwaseem857 <malikwaseem3802@gmail.com>
2026-01-01 18:50:39 +0500

    adding day_38

--- day_37/app.py
+++ day_37/app.py
@@ -1,2 +1,17 @@
 import pandas as pd
-import pandas profiling
+import streamlit as st
+import seaborn as sns
+
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+# Load dataset
+df = sns.load_dataset("titanic")
+
+# Create profile report
+profile = ProfileReport(df, explorative=True)
+
+# Streamlit app
+st.title("Titanic Dataset EDA Report")
+st_profile_report(profile)
+
